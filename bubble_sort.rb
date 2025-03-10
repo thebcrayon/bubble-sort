360802d99fd10a1d541c883af986bae0cd81e8c7
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2025-03-10 00:38:42 -0500

    Complete bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,16 @@
 def bubble_sort(array)
+  n = array.size-1
+  
+  array.each do |number|
+    n.times do |i|
+      if array[i] > array[i+1]
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+      end
+    end
+  end
+  
   array
 end
 
